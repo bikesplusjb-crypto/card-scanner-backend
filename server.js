f2c90f68545aac1c7dfae5c83930a0cb40438633
bikesplusjb-crypto <bikesplusjb@gmail.com>
2026-05-13 10:42:05 -0400

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ const multer = require("multer");
 const fetch = require("node-fetch");
 
 const app = express();
-
+const cors = require('cors');
+app.use(cors({ origin: '*' }));
 app.use(cors());
 app.use(express.json({ limit: "25mb" }));
 app.use(express.urlencoded({ extended: true }));
